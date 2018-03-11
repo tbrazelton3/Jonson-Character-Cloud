085e97e1fed815cd6bb87b14777de9a177893540
Coblentz, David <david.coblentz@emc.com>
2018-03-11 14:56:29 -0400

    add word counts

---
+++ src/plays/bartholomew-fair/parselines.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+// if ( typeof String.prototype.startsWith != 'function' ) {
+//   String.prototype.startsWith = function( str ) {
+//     return this.substring( 0, str.length ) === str;
+//   }
+// };
+
+const play = require('./playlines.txt');
+
+counts = {}
+totalCount = 0;
+play.split('\n').forEach(element => {
+  // console.log(element);
+  if(element.startsWith('<name>')) {
+    let name = element.split('</name>')[0];
+    name = name.replace('<name>','');
+    if( !counts[name] ) {
+      counts[name] = 0;
+    }
+    let lines = element.split('</name')[1];
+    counts[name]+= lines.split(' ').length;
+    totalCount += lines.split(' ').length;
+  }
+});
+
+console.log(counts);
+console.log(totalCount);
